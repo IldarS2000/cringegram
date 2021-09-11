213ec73651a71a58143f180723cac4d78c0c3fa8
Ildar <45717260+Karantir73@users.noreply.github.com>
2021-09-12 00:08:16 +0300

    add counters to tables

---
+++ src/server/db/create_tables.sql
@@ -0,0 +1,45 @@
+create table cringegram.public.user
+(
+    id                 serial primary key,
+    phone_number       char(15)    not null unique,
+    name               varchar(20) not null unique,
+    avatar             bytea,
+    about_me           varchar(64),
+    post_count         int         not null,
+    subscription_count int         not null,
+    access_token       text        not null,
+    refresh_token      text        not null
+);
+
+create table cringegram.public.post
+(
+    id          serial primary key,
+    user_id     int   not null references cringegram.public.user (id),
+    photo       bytea not null,
+    description varchar(512),
+    like_count  int   not null
+);
+
+create table cringegram.public.subscription
+(
+    user_id       int not null references cringegram.public.user (id),
+    subscriber_id int not null references cringegram.public.user (id),
+
+    primary key (user_id, subscriber_id)
+);
+
+create table cringegram.public.like
+(
+    post_id int not null references cringegram.public.post (id),
+    user_id int not null references cringegram.public.user (id),
+
+    primary key (post_id, user_id)
+);
+
+create table cringegram.public.comment
+(
+    id      serial primary key,
+    post_id int          not null references cringegram.public.post (id),
+    user_id int          not null references cringegram.public.user (id),
+    comment varchar(128) not null
+);
