7e309126885633cc0a58300cac1226b9e2475d9b
Ildar <45717260+Karantir73@users.noreply.github.com>
2021-09-11 22:04:09 +0300

    add sql scripts

---
+++ src/server/db/utilities.sql
@@ -0,0 +1,14 @@
+select *
+from cringegram.public.user;
+
+select *
+from cringegram.public.post;
+
+select *
+from cringegram.public.subscription;
+
+select *
+from cringegram.public.like;
+
+select *
+from cringegram.public.comment;
