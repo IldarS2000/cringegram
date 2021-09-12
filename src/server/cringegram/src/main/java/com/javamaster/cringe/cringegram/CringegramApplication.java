fd425c54bccd35e29ec5b31f5142e06b69a90300
barnet30 <pashor031@gmail.com>
2021-09-12 15:30:42 +0300

    add backend

---
+++ src/server/cringegram/src/main/java/com/javamaster/cringe/cringegram/CringegramApplication.java
@@ -0,0 +1,13 @@
+package com.javamaster.cringe.cringegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CringegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CringegramApplication.class, args);
+    }
+
+}
